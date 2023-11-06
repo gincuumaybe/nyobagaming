836e8dc1109e34a1164e75cabc8d2da40d1e7f9b
gincuumaybe <christherber445@gmail.com>
2023-11-06 19:09:24 +0700

    second commit

--- screens/write.js
+++ screens/write.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import { Input, Heading, Center } from "native-base";
-import Button from '../components/button';
+// import React from 'react';
+// import { Input, Heading, Center } from "native-base";
+// import Button from '../components/button';
 
-const Write = () => {
+// const Write = () => {
+//   return (
+//     <>
+//       <Center 
+//         flex={1}
+//         bg="#FFEEDC"
+//       >
+//       <Input placeholder='Input'/>
+//       <Input placeholder='Input'/>
+//       <Input placeholder='Input'/>
+//       <Input placeholder='Input'/>
+//         {/* <Heading>kontol</Heading> */}
+//         <Button onPress/>
+//       </Center>
+//     </>
+//   );
+// };
+
+
+
+// export default Write;
+
+
+import React, { useState } from 'react';
+// import { View, Text, TextInput, Button } from 'react-native';
+import { View,Text, TextInput, Button, Container, Content, Form, Item, Input, Label } from 'native-base';
+
+const MyForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    // You can handle form submission here, for example, send the data to an API
+    console.log('Name:', name);
+    console.log('Email:', email);
+  };
+
   return (
-    <>
-      <Center 
-        flex={1}
-        bg="#FFEEDC"
-      >
-      <Input placeholder='Input'/>
-      <Input placeholder='Input'/>
-      <Input placeholder='Input'/>
-      <Input placeholder='Input'/>
-        {/* <Heading>kontol</Heading> */}
-        <Button onPress/>
-      </Center>
-    </>
+    <Container>
+      <Content>
+        <Form>
+          <Item stackedLabel>
+            <Label>Name</Label>
+            <Input
+              onChangeText={(text) => setName(text)}
+              value={name}
+            />
+          </Item>
+          <Item stackedLabel>
+            <Label>Email</Label>
+            <Input
+              onChangeText={(text) => setEmail(text)}
+              value={email}
+              keyboardType="email-address"
+            />
+          </Item>
+        </Form>
+
+        <Button
+          title="Submit"
+          onPress={handleSubmit}
+          disabled={!name || !email}
+        />
+      </Content>
+    </Container>
   );
 };
 
-
-
-export default Write;
+export default MyForm;
